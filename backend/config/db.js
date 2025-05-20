046dbf92e7587a36544d8b76ecd77d56672076d9
MohammedTK <monw2021@gmail.com>
2025-05-20 09:52:13 +0100

    new commit
    20.05.2025

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/Meubles", {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
-    console.log("MongoDB connecté !");
-  } catch (error) {
-    console.error(" Erreur de connexion à MongoDB:", error);
+    console.log(`✅ MongoDB connecté : ${conn.connection.host}`);
+  } catch (err) {
+    console.error("❌ Erreur de connexion MongoDB :", err);
     process.exit(1);
   }
 };
