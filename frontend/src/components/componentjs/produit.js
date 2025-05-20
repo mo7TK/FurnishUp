8f608a86e6c461e74f085005437ec7b0d971f55b
MohammedTK <monw2021@gmail.com>
2025-05-20 13:43:41 +0100

    cc

--- frontend/src/components/componentjs/produit.js
+++ frontend/src/components/componentjs/produit.js
@@ -11,7 +11,7 @@ const Produit = ({ produit }) => {
     <div className="produit-container">
       <div className="produit-image-container">
         <img
-          src={`https://furnishup-backend.onrender.com/ ${produit.images[0].replace(
+          src={`https://furnishup-backend.onrm/ ${produit.images[0].replace(
             /\\/g,
             "/"
           )}`}
